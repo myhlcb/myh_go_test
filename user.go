0ea9c81b29f34df0e4ddd369896a840ad54b19b1
S1mple <1029785965@qq.com>
2023-01-14 16:20:27 +0800

    feat():添加查询用户列表功能

--- user.go
+++ user.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net"
 )
 
@@ -35,8 +36,24 @@ func (this *User) Outline() {
 	this.server.BroadCast(this, "下线")
 }
 
+// 给当前user对应客户端发消息
+func (this *User) SendMsg(msg string, conn net.Conn) {
+	this.conn.Write([]byte(msg))
+}
+
 // 处理用户消息
 func (this *User) DoMessage(msg string) {
+	// 查询当前用户列表
+	if msg == "who" {
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			OnlineMsg := fmt.Sprintf("[%s]%s:在线", user.Addr, user.Name)
+			this.SendMsg(OnlineMsg, this.conn)
+
+		}
+		this.server.mapLock.Unlock()
+
+	}
 	this.server.BroadCast(this, msg)
 }
 func NewUser(conn net.Conn, server *Server) *User {
